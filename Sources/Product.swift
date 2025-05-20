d3c8031366fa1a84f40cb79f79d1006b39bd42f5
Carlos Cuevas <ccuevas@mdlive.com>
2025-05-20 14:47:39 -0700

    finished the shoping car singleton impl

--- Sources/Product.swift
+++ Sources/Product.swift
@@ -1,12 +1,28 @@
-class Product{
-    var name: String
-    var price: Double
-    var quantity: Int
+public class Product{
     
-    init(name:String, price:Double, quantity:Int){
-        self.name=name
-        self.price=price
-        self.quantity=quantity
+    private let name: String
+    private var price: Double
+    private var quantity: Int
+    
+    init(name:String, price:Double){
+        self.name = name
+        self.price = price
+        self.quantity = 1
     }
     
+    func getName() -> String {
+        return self.name
+    }
+    
+    func getPrice() -> Double {
+        return self.price
+    }
+    
+    func getQuantity() -> Int {
+        return self.quantity
+    }
+    
+    func updateQuantity(_ quantity: Int) {
+        self.quantity = quantity
+    }
 }
--- Sources/ShoppingCartSingleton.swift
+++ Sources/ShoppingCartSingleton.swift
@@ -9,13 +9,44 @@ class ShoppingCartSingleton {
         return sharedInstance!
     }
     
-    func addProductToCart(_ product: Product) {
-        productList.append(product)
+    func addProduct(_ product: Product, quantity: Int = 1) {
+        if let index = findProductIndex(product.getName()) {
+            productList[index].updateQuantity(productList[index].getQuantity() + quantity)
+        } else {
+            productList.append(product)
+        }
     }
     
-    func removeProductFromCart(_ product: Product) {
-        if let index = productList.firstIndex(where: { $0.name == product.name }) {
+    func totallyRemoveProductFromCart(_ product: Product) {
+        if let index = findProductIndex(product.getName()) {
             productList.remove(at: index)
         }
+    }
+    
+    func removeProductFromCart(_ product: Product, quantity: Int = 1) {
+        if let index = findProductIndex(product.getName()) {
+            if productList[index].getQuantity() <= quantity {
+                totallyRemoveProductFromCart(product)
+            }else{
+                productList[index].updateQuantity(productList[index].getQuantity() - quantity)
+            }
+            
+        }
+    }
+    
+    func clearCart() {
+        productList.removeAll()
+    }
+    
+    func getTotalPrice() -> Double {
+        var total: Double = 0
+        for product in productList {
+            total = product.getPrice() * Double(product.getQuantity())
+        }
+        return total
+    }
+    
+    private func findProductIndex(_ productName: String) -> Int? {
+        return productList.firstIndex(where: { $0.getName() == productName})
     }
 }
